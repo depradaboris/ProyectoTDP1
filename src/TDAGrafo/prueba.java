2f934927d9d4615e82623f5332fa2b3ed40dca25
depradaboris <depradaboris19@gmail.com>
2020-09-17 17:38:04 -0300

    prueba.java and nodo.java added

---
+++ src/TDAGrafo/prueba.java
@@ -0,0 +1,17 @@
+package TDAGrafo;
+import TDAGrafoDirigido.*;
+public class prueba {
+	private int numerito;
+	private String nombre;
+	
+	public prueba(String nombre, int num) {
+		numerito=num;
+		this.nombre=nombre;
+	}
+	public int getNumerito() {
+		return numerito;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+}
